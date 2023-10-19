4590994759a5a698e2baa5ce3a65ea12d9810747
Your Name <you@example.com>
2023-10-19 07:44:42 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import {Card} from "react-bootstrap"
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
-  );
-}
+
+
+const App=()=>{
+  const cardInfo=[
+  {
+    image: "https://i.insider.com/50f967f56bb3f7830a000019",
+    title: "rajesh",
+    text: "koi bhi"
+  },
+  {
+    image: "https://i.insider.com/50f967f56bb3f7830a000019",
+    title: "kumar",
+    text: "wadhwani"
+  },
+  {
+    image: "https://i.insider.com/50f967f56bb3f7830a000019",
+    title: "koi bhi",
+    text: "wadhwani"
+  },
+  ];
+  const renderCard=(card,index)=>{
+    return(
+      <Card style={{ width: '18rem' }} key={index} >
+      <Card.Img variant="top" src={card.image} />
+      <Card.Body>
+        <Card.Title>{card.title}</Card.Title>
+        <Card.Text>
+          {card.text}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+        
+    )
+
+  }
+
+     return(
+      <React.Fragment>
+        
+      {cardInfo.map(renderCard)}
+        </React.Fragment>
+
+
+     );
+
+
+};
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+ 
+      
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-var React = require('react');
-var ReactDOM=require('react-dom');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from "./App";
+import "./index.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<h1> Hello World!</h1>
-,document.getElementById('root'));
+
+const rootElement=document.getElementById('root');
+ReactDOM.render(
+
+<App />,
+
+
+
+rootElement
+);
