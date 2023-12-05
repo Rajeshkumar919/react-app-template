2ba2c1f86002d3af4d020e4866a883eb9c08361d
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-12-05 12:33:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,65 +1,17 @@
-import React from "react";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import {Card} from "react-bootstrap"
-import Button from 'react-bootstrap/Button';
+// App.js
+import React from 'react';
+import { ProductProvider } from './component/ProductContext';
+import ProductList from './component/ProductList';
 
-
-
-const App=()=>{
-  const cardInfo=[
-  {
-    image: "https://i.insider.com/50f967f56bb3f7830a000019",
-    title: "rajesh",
-    text: "koi bhi"
-  },
-  {
-    image: "https://i.insider.com/50f967f56bb3f7830a000019",
-    title: "kumar",
-    text: "wadhwani"
-  },
-  {
-    image: "https://i.insider.com/50f967f56bb3f7830a000019",
-    title: "koi bhi",
-    text: "wadhwani"
-  },
-  ];
-  const renderCard=(card,index)=>{
-    return(
-      <Card style={{ width: '18rem' }} key={index} >
-      <Card.Img variant="top" src={card.image} />
-      <Card.Body>
-        <Card.Title>{card.title}</Card.Title>
-        <Card.Text>
-          {card.text}
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-        
-    )
-
-  }
-
-     return(
-      <React.Fragment>
-        
-      {cardInfo.map(renderCard)}
-        </React.Fragment>
-
-
-     );
-
-
+const App = () => {
+  return (
+    <ProductProvider>
+      <div>
+        <h1>Product List</h1>
+        <ProductList />
+      </div>
+    </ProductProvider>
+  );
 };
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
- 
-      
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
